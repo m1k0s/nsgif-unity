79879f8ea7bbdbf90afef71db1b199fcc8216620
Michael Steliaros <mike@steliaros.com>
2024-02-03 11:28:55 +0000

    Moved into the NSGIF namespace so it is easier to use with asmdefs

--- Assets/NSGIF/NSGIF.cs
+++ Assets/NSGIF/NSGIF.cs
@@ -2,142 +2,145 @@ using UnityEngine;
 using System;
 using System.Runtime.InteropServices;
 
-public class NSGIF : IDisposable
+namespace NSGIF
 {
-	public int frameCount { get; private set; }
-	public int frameIndex { get; private set; }
-
-	public void Dispose()
+	public class NSGIF : IDisposable
 	{
-		Dispose(true);
-		GC.SuppressFinalize(this);
-	}
+		public int frameCount { get; private set; }
+		public int frameIndex { get; private set; }
 
-	protected virtual void Dispose(bool disposing)
-	{
-		if (handle != IntPtr.Zero)
+		public void Dispose()
 		{
-			NSGIF_Destroy(handle);
-			handle = IntPtr.Zero;
+			Dispose(true);
+			GC.SuppressFinalize(this);
 		}
 
-		if (null != frameTexture)
+		protected virtual void Dispose(bool disposing)
 		{
-			GameObject.DestroyImmediate(frameTexture);
-			frameTexture = null;
-		}
-
-		if (disposing)
-		{
-			// No managed stuff to dispose...
-		}
-	}
-
-	~NSGIF()
-	{
-		Dispose(false);
-	}
-
-	public NSGIF(string filename)
-	{
-		if (null == filename)
-		{
-			throw new NullReferenceException("Filename is null");
-		}
-
-		handle = NSGIF_Create();
-
-		if (IntPtr.Zero == handle)
-		{
-			throw new Exception("Failed to create NSGIF plugin");
-		}
-
-		int count;
-		Status status;
-		NSGIF_InitializeFile(handle, filename, out count, out status);
-
-		if (Status.OK != status)
-		{
-			Dispose();
-			throw new Exception($"Failed to initialise NSGIF for {filename}: {status.ToString()}");
-		}
-
-		if (0 == count)
-		{
-			Dispose();
-			throw new Exception($"Empty gif {filename}");
-		}
-
-		frameCount = count;
-		frameIndex = -1;
-	}
-
-	public (Texture2D frame, int delayMillis) DecodeNextFrame()
-	{
-		if (++frameIndex == frameCount)
-		{
-			frameIndex = 0;
-		}
-
-		Status status;
-		int width;
-		int height;
-		int delay; // Decoded frame delay (in 1/100th of a second as per GIF spec)
-		IntPtr frameData = NSGIF_Decode(handle, frameIndex, out width, out height, out delay, out status);
-
-		if(Status.OK != status)
-		{
-			throw new Exception($"Failed to decode frame {frameIndex}/{frameCount}: {status.ToString()}");
-		}
-
-		if (null == frameTexture)
-		{
-			frameTexture = new Texture2D(width, height, TextureFormat.RGBA32, false);
-			if (null == frameTexture)
+			if (handle != IntPtr.Zero)
 			{
-				throw new Exception($"Failed to create {width}x{height} Texture2D");
+				NSGIF_Destroy(handle);
+				handle = IntPtr.Zero;
+			}
+
+			if (null != frameTexture)
+			{
+				GameObject.DestroyImmediate(frameTexture);
+				frameTexture = null;
+			}
+
+			if (disposing)
+			{
+				// No managed stuff to dispose...
 			}
 		}
 
-		if (width != frameTexture.width || height != frameTexture.height)
+		~NSGIF()
 		{
-			throw new Exception($"Size mismatch {width}x{height} (was {frameTexture.width}x{frameTexture.height})");
+			Dispose(false);
 		}
 
-		frameTexture.LoadRawTextureData(frameData, width * height * 4);
-		frameTexture.Apply(false, false);
+		public NSGIF(string filename)
+		{
+			if (null == filename)
+			{
+				throw new NullReferenceException("Filename is null");
+			}
 
-		return (frameTexture, delay * 10);
-	}
+			handle = NSGIF_Create();
 
-	private enum Status
-	{
-		OK = 0,
-		InsufficientFrameData = -1,
-		FrameDataError = -2,
-		InsufficientData = -3,
-		DataError = -4,
-		InsufficientMemory = -5,
-		FrameNoDisplay = -6,
-		EndOfFrame = -7,
-		FileOpenFailure = -8,
-		FileMapFailure = -9,
-		DecodingFinished = -10,
-		Destroyed = -11
-	}
+			if (IntPtr.Zero == handle)
+			{
+				throw new Exception("Failed to create NSGIF plugin");
+			}
+
+			int count;
+			Status status;
+			NSGIF_InitializeFile(handle, filename, out count, out status);
+
+			if (Status.OK != status)
+			{
+				Dispose();
+				throw new Exception($"Failed to initialise NSGIF for {filename}: {status.ToString()}");
+			}
+
+			if (0 == count)
+			{
+				Dispose();
+				throw new Exception($"Empty gif {filename}");
+			}
+
+			frameCount = count;
+			frameIndex = -1;
+		}
+
+		public (Texture2D frame, int delayMillis) DecodeNextFrame()
+		{
+			if (++frameIndex == frameCount)
+			{
+				frameIndex = 0;
+			}
+
+			Status status;
+			int width;
+			int height;
+			int delay; // Decoded frame delay (in 1/100th of a second as per GIF spec)
+			IntPtr frameData = NSGIF_Decode(handle, frameIndex, out width, out height, out delay, out status);
+
+			if(Status.OK != status)
+			{
+				throw new Exception($"Failed to decode frame {frameIndex}/{frameCount}: {status.ToString()}");
+			}
+
+			if (null == frameTexture)
+			{
+				frameTexture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+				if (null == frameTexture)
+				{
+					throw new Exception($"Failed to create {width}x{height} Texture2D");
+				}
+			}
+
+			if (width != frameTexture.width || height != frameTexture.height)
+			{
+				throw new Exception($"Size mismatch {width}x{height} (was {frameTexture.width}x{frameTexture.height})");
+			}
+
+			frameTexture.LoadRawTextureData(frameData, width * height * 4);
+			frameTexture.Apply(false, false);
+
+			return (frameTexture, delay * 10);
+		}
+
+		private enum Status
+		{
+			OK = 0,
+			InsufficientFrameData = -1,
+			FrameDataError = -2,
+			InsufficientData = -3,
+			DataError = -4,
+			InsufficientMemory = -5,
+			FrameNoDisplay = -6,
+			EndOfFrame = -7,
+			FileOpenFailure = -8,
+			FileMapFailure = -9,
+			DecodingFinished = -10,
+			Destroyed = -11
+		}
 
 #if UNITY_IPHONE && !UNITY_EDITOR
-	private const string __importName = "__Internal";
+		private const string __importName = "__Internal";
 #else
-	private const string __importName = "nsgif";
+		private const string __importName = "nsgif";
 #endif
 
-	[DllImport(__importName)] extern private static IntPtr NSGIF_Create();
-	[DllImport(__importName)] extern private static void NSGIF_InitializeFile(IntPtr handle, string filename, out int frameCount, out Status status);
-	[DllImport(__importName)] extern private static void NSGIF_InitializeBuffer(IntPtr handle, IntPtr data, int size, out int frameCount, out Status status);
-	[DllImport(__importName)] extern private static void NSGIF_Destroy(IntPtr handle);
-	[DllImport(__importName)] extern private static IntPtr NSGIF_Decode(IntPtr handle, int frameIndex, out int width, out int height, out int delay, out Status status);
+		[DllImport(__importName)] extern private static IntPtr NSGIF_Create();
+		[DllImport(__importName)] extern private static void NSGIF_InitializeFile(IntPtr handle, string filename, out int frameCount, out Status status);
+		[DllImport(__importName)] extern private static void NSGIF_InitializeBuffer(IntPtr handle, IntPtr data, int size, out int frameCount, out Status status);
+		[DllImport(__importName)] extern private static void NSGIF_Destroy(IntPtr handle);
+		[DllImport(__importName)] extern private static IntPtr NSGIF_Decode(IntPtr handle, int frameIndex, out int width, out int height, out int delay, out Status status);
 
-	private IntPtr handle;
-	private Texture2D frameTexture;
-};
+		private IntPtr handle;
+		private Texture2D frameTexture;
+	};
+}
--- Assets/PlayGIF.cs
+++ Assets/PlayGIF.cs
@@ -1,81 +1,84 @@
 using UnityEngine;
 using System.Collections;
 
-[ExecuteAlways]
-public class PlayGIF : MonoBehaviour
+namespace NSGIF
 {
-	private static readonly int MAIN_TEX_ID = Shader.PropertyToID("_MainTex");
-
-	public string filename = "";
-
-	private Material gifMaterial;
-	private IEnumerator playback;
-	private NSGIF gif = null;
-
-	void OnEnable()
+	[ExecuteAlways]
+	public class PlayGIF : MonoBehaviour
 	{
-		gifMaterial = GetComponent<Renderer>()?.sharedMaterial;
+		private static readonly int MAIN_TEX_ID = Shader.PropertyToID("_MainTex");
 
-		if (null == gifMaterial || string.IsNullOrEmpty(filename))
-		{
-			this.enabled = false;
-			return;
-		}
+		public string filename = "";
 
-		try
+		private Material gifMaterial;
+		private IEnumerator playback;
+		private NSGIF gif = null;
+
+		void OnEnable()
 		{
-			string path = Application.streamingAssetsPath + "/" + filename;
+			gifMaterial = GetComponent<Renderer>()?.sharedMaterial;
+
+			if (null == gifMaterial || string.IsNullOrEmpty(filename))
+			{
+				this.enabled = false;
+				return;
+			}
+
+			try
+			{
+				string path = Application.streamingAssetsPath + "/" + filename;
 #if UNITY_ANDROID  && !UNITY_EDITOR
-			// On Android streaming assets path is in the compressed jar; use WWW to uncompress.
-			WWW www = new WWW(path);
-			while(!www.isDone)
-				;
-			gif = new NSGIF(www.bytes);
+				// On Android streaming assets path is in the compressed jar; use WWW to uncompress.
+				WWW www = new WWW(path);
+				while(!www.isDone)
+					;
+				gif = new NSGIF(www.bytes);
 #else
-			gif = new NSGIF(path);
-			//gif = new NSGIF(System.IO.File.ReadAllBytes(path));
+				gif = new NSGIF(path);
+				//gif = new NSGIF(System.IO.File.ReadAllBytes(path));
 #endif
 
-			playback = Play();
-			StartCoroutine(playback);
-		}
-		catch(System.Exception e)
-		{
-			Debug.LogError($"{GetType()}.OnEnable: {e}");
-			this.enabled = false;
-		}
-	}
-
-	void OnDisable()
-	{
-		if (null != playback)
-		{
-			StopCoroutine(playback);
-			playback = null;
+				playback = Play();
+				StartCoroutine(playback);
+			}
+			catch(System.Exception e)
+			{
+				Debug.LogError($"{GetType()}.OnEnable: {e}");
+				this.enabled = false;
+			}
 		}
 
-		if (null != gifMaterial)
+		void OnDisable()
 		{
-			gifMaterial.SetTexture(MAIN_TEX_ID, null);
-			gifMaterial = null;
+			if (null != playback)
+			{
+				StopCoroutine(playback);
+				playback = null;
+			}
+
+			if (null != gifMaterial)
+			{
+				gifMaterial.SetTexture(MAIN_TEX_ID, null);
+				gifMaterial = null;
+			}
+
+			if (null != gif)
+			{
+				gif.Dispose();
+				gif = null;
+			}
 		}
 
-		if (null != gif)
+		IEnumerator Play()
 		{
-			gif.Dispose();
-			gif = null;
-		}
-	}
+			(Texture2D frame, int delayMillis) = gif.DecodeNextFrame();
+			gifMaterial.SetTexture(MAIN_TEX_ID, frame);
 
-	IEnumerator Play()
-	{
-		(Texture2D frame, int delayMillis) = gif.DecodeNextFrame();
-		gifMaterial.SetTexture(MAIN_TEX_ID, frame);
-
-		while (true)
-		{
-			yield return new WaitForSeconds(delayMillis / 1000.0f);
-			(frame, delayMillis) = gif.DecodeNextFrame();
+			while (true)
+			{
+				yield return new WaitForSeconds(delayMillis / 1000.0f);
+				(frame, delayMillis) = gif.DecodeNextFrame();
+			}
 		}
 	}
 }
